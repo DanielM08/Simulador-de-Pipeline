fe570014737c4b60d0cdc92ad7ebea091d3f3743
danielmarx <danielmarx08@gmail.com>
2018-06-18 18:13:29 -0300

    Método para configuração dos estágios em cada ciclo

--- src/simulador_de_Pipeline/Main.java
+++ src/simulador_de_Pipeline/Main.java
@@ -1,5 +1,6 @@
 package simulador_de_Pipeline;
 
+import java.util.Deque;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,4 +12,42 @@ public class Main {
 	    
 	}
 
+	
+	/**
+	 * Retorna a configuração dos 5 estágios do pipeline do MIPS através do vetor Cores. 
+	 * Se uma posição está setada como 1, indica-se que aquele estágio está em funcionamento no ciclo.
+	 * @param inicioInst Indica o ciclo de início de cada instrução
+	 * @param nCiclo Indica em que ciclo 
+	 * @param Cores
+	 * @return
+	 */
+	public int[] vetorColorir(Deque<Integer> inicioInst, int nCiclo, int[] Cores)
+	{
+		
+		for(int i = 4; i > 0; i--)
+		{
+			if(Cores[i-1] == 1)
+			{
+				Cores[i] = 1;
+				Cores[i-1] = 0;
+			}
+			else
+				Cores[i] = 0;
+		}
+		
+		//Procura se alguma instrução inicia naquele ciclo
+		for(int i : inicioInst)
+		{
+		
+			if(nCiclo == i)
+			{
+				Cores[0] = 1;
+				break;
+			}
+		}
+		
+		return Cores;
+	}
+	
 }
+
